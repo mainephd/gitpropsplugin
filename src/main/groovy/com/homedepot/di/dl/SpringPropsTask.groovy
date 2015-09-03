55415ab8463bf6343e419cf3d8d754e68f69ffa6
TomG713 <thomasgamble2@gmail.com>
2015-09-02 21:35:26 -0400

    added wrapper and updated task name

--- src/main/groovy/com/homedepot/di/dl/SpringPropsTask.groovy
+++ src/main/groovy/com/homedepot/di/dl/SpringPropsTask.groovy
@@ -3,16 +3,15 @@ package com.homedepot.di.dl
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
-class generateSpringProperties extends DefaultTask {
+class SpringPropsTask extends DefaultTask {
 
-def repo
-def dir
-def file
+    def git = org.ajoberstar.grgit.Grgit.open(project.file('.'))
+    def lastCommit = git.head().abbreviatedId;
+    def gitProps = new Properties();
 
     @TaskAction
     generateGitProperties() {
         try {
-        def repo = org.ajoberstar.grgit.Grgit.open(project.file('.'))
         def dir = new File(project.buildDir, "resources/main")
         def file = new File(project.buildDir, "resources/main/git.properties")
         if (!dir.exists()) {
@@ -21,21 +20,16 @@ def file
         if (!file.exists()) {
             file.createNewFile()
         }
-        def map = ["git.branch"                : repo.branch.current.name
-                   , "git.commit.id"           : repo.head().id
-                   , "git.commit.id.abbrev"    : repo.head().abbreviatedId
-                   , "git.commit.user"         : repo.head().author.name
-                   , "git.commit.user.email"   : repo.head().author.email
-                   , "git.commit.message"      : repo.head().shortMessage
-                   , "git.commit.message.full" : repo.head().fullMessage
-                   , "git.commit.time"         : repo.head().date.toString()]
-        def props = new Properties()
-        props.putAll(map)
-        props.store(file.newWriter(), "")
+        def map = [  "git.commit.id"           : git.head().id
+                   , "git.commit.user"         : git.head().author.name
+                   , "git.commit.user.email"   : git.head().author.email
+                   , "git.commit.message"      : git.head().shortMessage
+                   , "git.commit.time"         : git.head().date.toString()
+                   , "git.branch"              : git.branch.current.name]
+        gitProps.putAll(map)
+        gitProps.store(file.newWriter(), "")
         } catch (e) {
-            project.logger.error('Unable to generate Git properties file.', e)
+            project.logger.error('Unable to update git.properties', e)
         }
     }
-
-
 }
